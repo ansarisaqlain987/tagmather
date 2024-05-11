16fd855b658288c4ef6657d0a5ef3e81abd4d5ef
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-05-11 15:05:36 +0530

    segregated the components

---
+++ src/app/dashboard/transactions/page.tsx
@@ -0,0 +1,7 @@
+export default function Trans() {
+    return (
+        <>
+            Hello WOrld
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Dashboard } from "./dash2";
-import { Dashboard1 } from "./dash1";
 
 export default function HomeComponent() {
-  return <Dashboard1 />
+  return <>Hello</>
 }
